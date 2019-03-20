1e7b3ec6b0e49c046d1e7153251fa0f19d98a12c
zaki@alodokter <soeltan.zaky@alodokter.com>
2019-03-20 16:36:17 +0700

    template

--- frontend/src/components/widget/ArticleHome.vue
+++ frontend/src/components/widget/ArticleHome.vue
@@ -1,5 +1,17 @@
 // JS fiddle : https://jsfiddle.net/eL4gvp1c/
 
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    title:'How to create application boilerplate with Vert.x, VueJS, and OAuth2 in five steps',
+    topics:'Javascript',
+    short_desc : '<p>In this tutorial you will learn how to setup an application boilerplate based on Vert.x (Java) as a backend and VueJs as a frontend with a focus on User Authentication against Keycloak through OAuth2.</p>',
+    image : 'https://res.cloudinary.com/dk0z4ums3/image/upload/w_667,h_336,c_fill/v1545292047/attached_image/alergi-si-kecil-sering-kambuh-bisa-jadi-karena-salah-minum-susu.jpg',
+    authors: 'Jack Cohen in OneZero',
+    date_created: 'Nov 16, 2018'
+  }
+})
 
 <article class="media content-article">
         <div class="media-content">
