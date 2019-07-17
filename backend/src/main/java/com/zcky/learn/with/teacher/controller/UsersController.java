09bd4d3755773fffed30b855234b620789ac3539
zaki@alodokter <soeltan.zaky@alodokter.com>
2019-07-17 16:58:53 +0700

    fix jwt

--- backend/src/main/java/com/zcky/learn/with/teacher/controller/UsersController.java
+++ backend/src/main/java/com/zcky/learn/with/teacher/controller/UsersController.java
@@ -1,5 +1,6 @@
 package com.zcky.learn.with.teacher.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,12 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,14 +28,22 @@ import com.zcky.learn.with.teacher.constant.Constant;
 import com.zcky.learn.with.teacher.mongoDb.model.Users;
 import com.zcky.learn.with.teacher.mongoDb.repository.UsersRepository;
 import com.zcky.learn.with.teacher.mongoDb.serializer.UsersSerializer;
+import com.zcky.learn.with.teacher.security.JwtTokenUtil;
 
 @RestController
 public class UsersController extends BaseController{
 
 	@Autowired
 	private UsersRepository repository;
-
-	@RequestMapping(value = "/users/sign_up", method = RequestMethod.POST)
+	
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+	
+	public UserDetails getUserDetails(Users user) {
+		return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+	}
+	
+	@RequestMapping(value = "/api/users/sign_up", method = RequestMethod.POST)
 	public ResponseEntity<String> signUp(@Valid @RequestBody Users user, HttpServletRequest request) throws Exception {
 		Users dataUser = repository.findByUserName(user.getUsername());
 		JsonObject response;
@@ -50,14 +65,16 @@ public class UsersController extends BaseController{
 		return new ResponseEntity<String>( response.toString(), getResponseHeader(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/users/sign_in", method = RequestMethod.POST)
+	@RequestMapping(value = "/api/users/sign_in", method = RequestMethod.POST)
 	public ResponseEntity<String> signIn(@Valid @RequestBody Users user, HttpServletResponse responseHeader) throws Exception {
 		JsonObject response;
 		try {
 			Users userResponse = repository.signIn(user.getUsername(), user.getPassword());
 			if(userResponse != null) {
 				response = getSuccessResponse();
+				final String token = jwtTokenUtil.generateToken(this.getUserDetails(userResponse));
 				response.add(Constant.RESPONSE, toJSONObjectWithSerializer(Users.class, new UsersSerializer(), userResponse) );
+				response.addProperty("token", token);
 			}else {
 				response = getFailedResponse();
 				response.addProperty(Constant.ERROR_MESSAGE,Constant.USER_NOT_FOUND_ERROR_MESSAGE);
@@ -69,7 +86,7 @@ public class UsersController extends BaseController{
 		return new ResponseEntity<String>( response.toString(), getResponseHeader(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/users/edit_user", method = RequestMethod.PUT)
+	@RequestMapping(value = "/api/users/edit_user", method = RequestMethod.PUT)
 	public ResponseEntity<String> editUsers(@Valid @RequestBody Users user, HttpServletRequest request){
 		JsonObject response;
 		try {
@@ -84,7 +101,7 @@ public class UsersController extends BaseController{
 		return new ResponseEntity<String>( response.toString(), getResponseHeader(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/users/find_all_users", method = RequestMethod.GET)
+	@RequestMapping(value = "/api/users/find_all_users", method = RequestMethod.GET)
 	public ResponseEntity<String> finddAllUsers(HttpServletRequest request){
 		JsonObject response;
 		try {
@@ -98,7 +115,7 @@ public class UsersController extends BaseController{
 		return new ResponseEntity<String>( response.toString(), getResponseHeader(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/users/delete_user/{id}", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/api/users/delete_user/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<String> deleteUsers(@PathVariable String id, HttpServletRequest request){
 		JsonObject response;
 		try {
--- backend/src/main/java/com/zcky/learn/with/teacher/security/JwtUserDetailsService.java
+++ backend/src/main/java/com/zcky/learn/with/teacher/security/JwtUserDetailsService.java
@@ -2,22 +2,35 @@ package com.zcky.learn.with.teacher.security;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.zcky.learn.with.teacher.mongoDb.model.Users;
+import com.zcky.learn.with.teacher.mongoDb.repository.UsersRepository;
+
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UsersRepository repository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		if ("maribelajarbersama".equals(username)) {
-			return new User("maribelajarbersama", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+		Users user = repository.findByUserName(username);
+		if(user != null) {
+			return new User(user.getUsername(), user.getPassword(),
 					new ArrayList<>());
-		} else {
-			throw new UsernameNotFoundException("User not found with username: " + username);
+		}else {
+			if ("maribelajarbersama".equals(username)) {
+				return new User("maribelajarbersama", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+						new ArrayList<>());
+			} else {
+				throw new UsernameNotFoundException("User not found with username: " + username);
+			}
 		}
 	}
 }
