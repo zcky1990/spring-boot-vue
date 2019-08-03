f101f46bb5b77298449c7a5a11a4be0eec4bce87
Zaki Rizaldy <zcky1990@gmail.com>
2019-08-03 23:44:37 +0700

    add users layout

---
+++ frontend/src/layout/UsersLayout.vue
@@ -0,0 +1,46 @@
+    <template>
+        <div class="user-page-container">
+            <section class="nav-section">
+                <user-nav-menu></user-nav-menu>
+            </section>
+            <section class="content-section container">
+                <v-content>
+      <v-container fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          asdasdasdsaad
+        </v-layout>
+      </v-container>
+    </v-content>
+            </section>
+        </div>
+    </template>
+
+    <script>
+
+    import Navbar from '@/components/widget/UserNavbar'
+
+    export default   {
+        name: 'user-page-layout',
+        data(){
+            return {
+
+            }
+        },
+        components: {
+                    'user-nav-menu' : Navbar,
+        }
+    }
+    </script>
+
+    <!-- Add "scoped" attribute to limit CSS to this component only -->
+    <style scoped>
+        .sign-up-page-container {
+            background-color: white;
+        }
+        .content-section {
+            padding-top: 45px;
+        }
+    </style>
--- frontend/src/pages/User.vue
+++ frontend/src/pages/User.vue
@@ -1,86 +1,16 @@
 <template>
-    <div class="user-page">
-        Success Login {{userData.username}}
-    </div>
+    <layout-page></layout-page>
 </template>
 
 <script>
-import {AXIOS} from '@/components/http-common'
-import {Util} from '@/components/util'
+import Layout from '@/layout/UsersLayout'
 
 export default {
-    name: 'index',
-    props: {
-        msg: String
+    name: 'user-page',
+    components: {
+        'layout-page' : Layout
     },
-    data () {
-        return {
-            headers : {},
-            users : {}
-        }
-    },
-    beforeCreate: function () {
-        Util.checkUserSession(this.$session, this.$router);
-    },
-    created() {
-        this.getUsersDetails();
-    },
-    methods: {
-        getRequestHeader : function(){
-            this.requestHeader = Util.getHeaders(this.$session);
-            return this.requestHeader;
-        },
-        getUsersDetails : function(){
-            let id = this.$session.get('uid');
-            let headers =  this.getRequestHeader();
-            let self = this;
-            AXIOS.get('/users/get_user_detail/'+id, {headers} )
-            .then(response => {
-                if(response.status == 200){
-                    let responseData = response.data
-                    self.userData = responseData.response;
-                    }
-                })
-            .catch(e => {
-                self.errors.push(e)
-            }) 
-        }
-    },
-    computed: {
-        requestHeader: {
-               get : function(){
-                   return this.headers;
-               },
-               set: function(header){
-                   this.headers = header;
-                }
-        },
-        userData : {
-            get: function(){
-                return this.users;
-            },
-            set: function (userdata){
-                this.users = userdata;
-            }
-        }
-    }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h3 {
-    margin: 40px 0 0;
-    }
-    ul {
-    list-style-type: none;
-    padding: 0;
-    }
-    li {
-    display: inline-block;
-    margin: 0 10px;
-    }
-    a {
-    color: #42b983;
-    }
 </style>
