37bb910906cfa400eda6039968c30cf0c2c9d994
zaki@alodokter <soeltan.zaky@alodokter.com>
2019-08-06 16:59:38 +0700

    fix article css

--- frontend/src/components/widget/Article.vue
+++ frontend/src/components/widget/Article.vue
@@ -2,11 +2,43 @@
   <section class="section article">
     <v-container id="grid" fluid grid-list-sm tag="section">
       <v-layout wrap>
-        <v-flex d-flex xs12>
-          
-        </v-flex>
+        
         <v-flex tag="h1" class="headline">{{article.article_title}}</v-flex>
-        <v-flex d-flex xs12 order-xs5>
+
+        <v-flex d-flex xs12>
+          <v-container>
+            <v-layout>
+              <div class="image">
+                <v-img
+                  src="https://picsum.photos/500/300?image=11"
+                  lazy-src="https://picsum.photos/10/6?image=11"
+                  aspect-ratio="1"
+                  class="grey lighten-2 rounded"
+                >
+                  <template v-slot:placeholder>
+                    <v-layout
+                      fill-height
+                      align-center
+                      justify-center
+                      ma-0
+                    >
+                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                    </v-layout>
+                  </template>
+                </v-img>
+
+              </div>
+              <v-flex>
+                <div class="author">
+                  <div class="authors-name">{{article.author.name}}</div>
+                  <div class="article-create-date">{{article.created_date}}</div>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+
+        <v-flex d-flex xs12 order-xs5 class="content">
           <v-layout column>
             <v-flex>
               <v-card flat>
@@ -47,7 +79,6 @@ export default {
     getArticleService: function() {
       let self = this;
       let headers = Util.getHeaders(this.$session);
-      console.log(this.getArticleUrl);
       AXIOS.get(this.getArticleUrl + this.slug, { headers })
         .then(response => {
           if (response.status == 200) {
@@ -62,7 +93,27 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.article-content {
-  text-align: left;
+.author {
+  padding-left: 20px;
+  padding-top: 7px;
+}
+.rounded {
+  border-radius: 66px;
+    width: 64px;
+    height: 64px;
+}
+.authors-name {
+    font-size: 1.2em;
+}
+.article-create-date {
+  color: grey;
+}
+
+.headline {
+  font-size: 40px !important;
+}
+.content {
+  margin-top: 1.95em;
+  font-size: 20px;
 }
 </style>
