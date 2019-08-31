c7cbfab5ea6b57e426c72f2ac68b39ea2436bb3d
Zaki Rizaldy <zcky1990@gmail.com>
2019-08-31 13:28:43 +0700

    add eventbus

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,16 +4,47 @@
     This component is used for dynamically managing your content area 
     and is the mounting point for many components.-->
     <v-app class="app-container" id="content-container">
+      <div class="snack-bar-container">
+        <snack-bar ref="snackbar" @showSnackBar="setMessage()"></snack-bar>
+      </div>
       <router-view></router-view>
     </v-app>
   </div>
 </template>
 
 <script>
+import SnackBar from "@/components/widget/SnackBar";
+import { EventBus } from './EventBus.js';
+
 export default {
   name: "app",
   data() {
-    return {};
+    return {
+      snackBarConfig: {
+        color: "error",
+        timeout: 6000,
+        top: true
+      }
+    };
+  },
+  created(){
+    EventBus.$on('SNACKBAR_TRIGGERED', (val) => {
+      this.setMessage(val.message, val.type);
+    })
+  },
+  components: {
+    "snack-bar": SnackBar
+  },
+  methods: {
+    setMessage: function(message, type) {
+      if(type == 0){
+        this.snackBarConfig.color = "success"
+      }else{
+        this.snackBarConfig.color = "error"
+      }
+      this.$refs.snackbar.setConfig(this.snackBarConfig);
+      this.$refs.snackbar.showSnackbar(message);
+    },
   }
 };
 </script>
--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -4,29 +4,41 @@
 
     <script>
     import Layout from '@/layout/IndexLayout'
+    import { EventBus } from './../EventBus.js';
 
     export default {
     name: 'index',
     components: {
         'index-layout-page' : Layout
         },
+        created(){
+           this.setMessage("asdw", 1);
+        },
+        methods: {
+            setMessage: function(message, type){
+                let data={}
+                data.message = message
+                data.type = type
+                    EventBus.$emit('SNACKBAR_TRIGGERED', data)
+            }
+        }
     }
     </script>
 
     <!-- Add "scoped" attribute to limit CSS to this component only -->
     <style scoped>
     h3 {
-    margin: 40px 0 0;
+        margin: 40px 0 0;
     }
     ul {
-    list-style-type: none;
-    padding: 0;
+        list-style-type: none;
+        padding: 0;
     }
     li {
-    display: inline-block;
-    margin: 0 10px;
+        display: inline-block;
+        margin: 0 10px;
     }
     a {
-    color: #42b983;
+        color: #42b983;
     }
     </style>
