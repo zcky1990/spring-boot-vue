8bc9cc191a8ffc2faed7b8d3bec9bd5ee4dd0a68
zaki@alodokter <soeltan.zaky@alodokter.com>
2019-10-03 16:00:50 +0700

    fix load font

--- backend/src/main/java/app/environtment/Environtment.java
+++ backend/src/main/java/app/environtment/Environtment.java
@@ -6,7 +6,7 @@ public class Environtment {
 	private String environtment;
 	
 	public Environtment() {
-		environtment = Constant.DEV_ENV;
+		environtment = Constant.PRODUCTION_ENV;
 	}
 
 	public String getEnvirontment() {
